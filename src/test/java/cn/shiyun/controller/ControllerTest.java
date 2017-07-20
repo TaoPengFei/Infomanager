dc03c5eaa08c15c193ed04044b3992288856200b
TaoPengFei <302665611@qq.com>
2017-07-20 14:11:52 +0800

    add JUnitTest

---
+++ src/test/java/cn/shiyun/controller/ControllerTest.java
@@ -0,0 +1,7 @@
+package cn.shiyun.controller;
+
+/**
+ * Created by 陶鹏飞 on 2017/7/20.
+ */
+public class ControllerTest {
+}
---
+++ src/test/java/cn/shiyun/entity/Entity.java
@@ -0,0 +1,7 @@
+package cn.shiyun.entity;
+
+/**
+ * Created by 陶鹏飞 on 2017/7/20.
+ */
+public class Entity {
+}
