9d039457a4503ccd117d6c83cf0657d1b9d7c75d
TaoPengFei <302665611@qq.com>
2017-07-25 18:04:07 +0800

    主页的UI优化...

---
+++ sql/grant_all.sql
@@ -0,0 +1,4 @@
+GRANT ALL PRIVILEGES ON hibernate.* to 'guest'@'%' IDENTIFIED BY '111111' WITH GRANT OPTION;
+flush privileges;
+GRANT ALL PRIVILEGES ON bi.* to 'guest'@'%' IDENTIFIED BY '111111' WITH GRANT OPTION;
+flush privileges;
