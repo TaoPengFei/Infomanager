bf82a5a535c9bb124be824b3b4986af56aaaa351
TaoPengFei <302665611@qq.com>
2017-09-27 14:21:34 +0800

    预防提交...

--- src/main/webapp/org/cboard/controller/config/dateCtrl.js
+++ src/main/webapp/org/cboard/controller/config/dateCtrl.js
@@ -152,4 +152,22 @@ cBoard.controller('dateCtrl', function ($rootScope, $scope, $http, dataService, 
         });
     }
 
+    var datepicker1 = $('#datetimepicker1').datetimepicker({
+        format: 'YYYY-MM-DD'
+        // locale: moment.locale('zh-cn')
+    }).on('dp.change', function (e) {
+        var result = new moment(e.date).format('YYYY-MM-DD');
+        $scope.dateOne = result;
+        $scope.$apply();
+    });
+
+    $('#datetimepicker2').datetimepicker({
+        format: 'YYYY年MM月DD日 hh:mm'
+        // locale: moment.locale('zh-cn')
+    }).on('dp.change', function (e) {
+        var result = new moment(e.date).format('YYYY年MM月DD日 hh:mm');
+        $scope.dateTwo= result;
+        $scope.$apply();
+    });
+
 });
