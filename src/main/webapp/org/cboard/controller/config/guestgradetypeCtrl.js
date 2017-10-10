b9c97e21e614470c0bf8170baac2a9ef8d682f8c
TaoPengFei <302665611@qq.com>
2017-10-10 11:06:30 +0800

    顾客档次上下限区间设定...

--- src/main/webapp/org/cboard/controller/config/guestgradetypeCtrl.js
+++ src/main/webapp/org/cboard/controller/config/guestgradetypeCtrl.js
@@ -9,15 +9,17 @@ cBoard.controller('guestgradetypeCtrl', function ($rootScope, $scope, $http, dat
     $scope.guestgradetypeHeaderInfos = [
         {'name': '档次名称', 'col': 'GuestGradeTypeName'},
         {'name': '档次描述', 'col': 'GuestGradeTypeDesc'},
-        {'name': '档次上限', 'col': 'TopAmt'},
         {'name': '档次下限', 'col': 'BottomAmt'},
+        {'name': '档次上限', 'col': 'TopAmt'},
         {'name': '档次状态', 'col': 'Status'},
         {'name': '创建时间', 'col': 'CreateTime'},
         {'name': '更新时间', 'col': 'UpdateTime'},
         {'name': '操作'}
     ];
 
-    //初始化
+    /**
+     * 初始化
+     */
     var getGuestGradeTypeList = function () {
         $http({
             method: 'get',
@@ -27,12 +29,66 @@ cBoard.controller('guestgradetypeCtrl', function ($rootScope, $scope, $http, dat
              }*/
         }).success(function (response) {
             $scope.guestGradeTypeList = response;
+            $scope.intervalCheck = "";
+            console.log(response.data);
+            /**
+             * 算法功能：判断不同的档次的上下限的值的设定是否合法（区间的交叉或重叠、区间的相离）;
+             * 算法思路：把下限，上限分别存进两个数组，并用sort排序，循环遍历数组，从下限的第二个元素去比较上限值
+             */
+            var startArr = [];
+            var endArr = [];
+            // var intervalArr = [];
+            for (var i = 0, len = response.data.length; i < len; i++) {
+                startArr.push(response.data[i].BottomAmt);
+                /*startArr.push({
+                    GuestGradeTypeId: response.data[i].GuestGradeTypeId,
+                    BottomAmt: response.data[i].BottomAmt
+                });*/
+                endArr.push(response.data[i].TopAmt);
+                /*endArr.push({
+                    GuestGradeTypeId: response.data[i].GuestGradeTypeId,
+                    TopAmt: response.data[i].TopAmt
+                });*/
+            }
+            /*console.log(startArr);
+            console.log(endArr);
+            for (var k = 1; k < startArr.length; k++) {
+                if (Number(startArr[k].BottomAmt) < Number(endArr[k - 1].TopAmt)) {
+                    // alert("档次的上下限存在重叠！");
+                    $scope.intervalCheck = "1";
+                    /!*intervalArr.push(startArr[k].GuestGradeTypeId);
+                    intervalArr.push(endArr[k - 1].GuestGradeTypeId);*!/
+                    return false;
+                }
+            }*/
+            // console.log(intervalArr);
+            /*var begin = startArr.sort();
+             var over = endArr.sort();*/
+            var begin = startArr.sort();
+            var over = endArr.sort();
+            console.log(begin);
+            console.log(over);
+            for (var k = 1; k < begin.length; k++) {
+                if (begin[k] < over[k - 1]) {
+                    alert("档次的上下限存在重叠！");
+                    $scope.intervalCheck = "1";
+                    return false;
+                }
+            }
+
+            // do something...
+
         }).error(function (XMLHttpRequest, textStatus, errorThrown) {
             ModalUtils.alert(translate(errorThrown + "!"), "modal-danger", "sm");
         });
     };
     getGuestGradeTypeList();
-    
+
+    /**
+     * 添加
+     * @param current
+     * @param $event
+     */
     $scope.addGuestGradeType = function (current, $event) {
         $uibModal.open({
             templateUrl: 'org/cboard/view/config/modal/addGuestGradeType.html',
@@ -72,6 +128,11 @@ cBoard.controller('guestgradetypeCtrl', function ($rootScope, $scope, $http, dat
         });
     }
 
+    /**
+     * 修改
+     * @param current
+     * @param $event
+     */
     $scope.editGuestGradeType = function (current, $event) {
         $uibModal.open({
             templateUrl: 'org/cboard/view/config/modal/editGuestGradeType.html',
@@ -117,6 +178,11 @@ cBoard.controller('guestgradetypeCtrl', function ($rootScope, $scope, $http, dat
         });
     }
 
+    /**
+     * 删除
+     * @param current
+     * @param $event
+     */
     $scope.delGuestGradeType = function (current, $event) {
         $http({
             method: 'POST',
